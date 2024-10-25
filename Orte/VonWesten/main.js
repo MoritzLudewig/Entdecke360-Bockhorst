ab63f934cc344e5acd4001baae4bca365a71989a
MoritzLudewig <ludewig.biz@gmail.com>
2024-10-25 17:55:52 +0200

    Add files via upload

--- Orte/VonWesten/main.js
+++ Orte/VonWesten/main.js
@@ -5,6 +5,7 @@ const imageContainer = document.querySelector(".image-container");
 
 const viewer = new PANOLENS.Viewer({
     container: imageContainer,
+    controlBar: false,
 });
 
 viewer.add(panoramaImage);
